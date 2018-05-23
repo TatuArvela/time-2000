bcb4169877d5d67c17ec0a32e6ed3cb1a9a42010
Tatu Arvela <tatu.arvela@gmail.com>
2018-05-23 20:05:19 +0300

    This software is entirely bug-free and beautiful, and it was written by a very stable genius!

--- index.js
+++ index.js
@@ -1,49 +1,61 @@
 function getValue(id) {
   var value = document.getElementById(id).value;
-  return (value != "" ? value : "00:00");
+  return (value != '' ? value : '00:00');
 }
 
 function subtractTime(time1, time2) {
-  return moment.utc(moment(time1, "HH:mm").diff(moment(time2, "HH:mm"))).format("HH:mm");
+  return moment.utc(moment(time1, 'HH:mm').diff(moment(time2, 'HH:mm'))).format('HH:mm');
 }
 
 function inputChangeHandler() {
-  var output = subtractTime(getValue("stop"), getValue("start"));
-  output = subtractTime(output, getValue("lunch"));
-  output = subtractTime(output, getValue("breaks"));
-  output = subtractTime(output, getValue("task1"));
-  output = subtractTime(output, getValue("task2"));
-  output = subtractTime(output, getValue("task3"));
-  output = subtractTime(output, getValue("task4"));
-  output = subtractTime(output, getValue("task5"));
-  output = subtractTime(output, getValue("task6"));
+  var output = subtractTime(getValue('stop'), getValue('start'));
+  output = subtractTime(output, getValue('lunch'));
+  output = subtractTime(output, getValue('breaks'));
+  output = subtractTime(output, getValue('task1'));
+  output = subtractTime(output, getValue('task2'));
+  output = subtractTime(output, getValue('task3'));
+  output = subtractTime(output, getValue('task4'));
+  output = subtractTime(output, getValue('task5'));
+  output = subtractTime(output, getValue('task6'));
 
-  document.getElementById("unregistered").value = output;
+  document.getElementById('unregistered').value = output;
 }
 
 inputChangeHandler();
 
-var inputs = document.getElementsByTagName("input");
+var inputs = document.getElementsByTagName('input');
 for (var input in inputs) {
   if (inputs[input].addEventListener != null)
-    inputs[input].addEventListener("change", inputChangeHandler);
+    inputs[input].addEventListener('change', inputChangeHandler);
 }
 
-$( "#sakura-window" ).draggable({ containment: "body", scroll: false });
+$( '#sakura-window' ).draggable({ containment: 'body', scroll: false });
 
-$( "#switcher ").change(function() {
+$( '#switcher ').change(function() {
   switch($(this).val()) {
-    case "Plain":
-      $("#theme-vapor").prop( "disabled", true );
-      $("#theme-extra").prop( "disabled", true );
+    case 'Plain':
+      $('#theme-vapor').prop( 'disabled', true );
+      $('#theme-extra').prop( 'disabled', true );
       break;
-    case "Calm":
-      $("#theme-vapor").prop( "disabled", false );
-      $("#theme-extra").prop( "disabled", true );
+    case 'Calm':
+      $('#theme-vapor').prop( 'disabled', false );
+      $('#theme-extra').prop( 'disabled', true );
       break;
-    case "Fantastic":
-      $("#theme-vapor").prop( "disabled", false );
-      $("#theme-extra").prop( "disabled", false );
+    case 'Fantastic':
+      if ( !$( '#theme-extra' ).length )
+        appendFantastic();
+      $('#theme-vapor').prop( 'disabled', false );
+      $('#theme-extra').prop( 'disabled', false );
       break;
   }
 })
+
+function appendFantastic() {
+  var extra = document.createElement('link');
+  extra.rel = 'stylesheet';
+  extra.type = 'text/css';
+  extra.href = 'extra.css';
+  extra.id = 'theme-extra';
+  extra.disabled = true;
+  $( 'head' ).append(extra);
+}
