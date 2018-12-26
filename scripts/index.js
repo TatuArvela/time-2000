53f63be11f05433efa9023241c130fc2a3fa572c
Tatu Arvela <tatu.arvela@gmail.com>
2018-12-27 01:29:20 +0200

    Full theme rewrite and other tweaks

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,56 @@
+/*
+  Shell
+*/
+
+function makeWindowActive() {
+  $('.active').removeClass('active');
+  $(this).addClass('active');
+}
+
+$(document).ready(function() {
+  $('.window').draggable({
+    start: makeWindowActive,
+    handle: 'header',
+    containment: 'body'
+  });
+  
+  $('.window').click(makeWindowActive)  
+
+  $('#sakura-time').resizable({
+    start: makeWindowActive,
+    containment: 'body'
+  });
+});
+
+
+/*
+  Theme switcher
+*/
+
+function setTheme(theme) {
+  $('#theme').prop('href', 'themes/' + theme +'/theme.css');
+}
+
+$('#theme-selector').change(function() {
+  if ($(this).val()) {
+    $('#theme').prop('disabled', false);
+    setTheme($(this).val());
+  }
+  else {
+    $('#theme').prop('disabled', true);
+    setTheme('');
+  }
+})
+
+$(document).ready(function() {
+  setTheme($('#theme-selector').val())
+});
+
+
+/*
+  Sakura Time 95
+*/
+
 function getValue(id) {
   var value = document.getElementById(id).value;
   return (value != '' ? value : '00:00');
@@ -29,7 +82,6 @@ function inputChangeHandler() {
     var timeDifference = subtractTime(totalTime, getValue('planned'));
   }
 
-
   document.getElementById('total').value = totalTime;
   document.getElementById('unlogged').value = unloggedTime;
   document.getElementById('difference').value = timeDifference;
@@ -41,35 +93,4 @@ var inputs = document.getElementsByTagName('input');
 for (var input in inputs) {
   if (inputs[input].addEventListener != null)
     inputs[input].addEventListener('change', inputChangeHandler);
-}
-
-$( '#sakura-window' ).draggable({ containment: 'body', scroll: false });
-
-$( '#switcher ').change(function() {
-  switch($(this).val()) {
-    case 'Plain':
-      $('#theme-vapor').prop( 'disabled', true );
-      $('#theme-extra').prop( 'disabled', true );
-      break;
-    case 'Calm':
-      $('#theme-vapor').prop( 'disabled', false );
-      $('#theme-extra').prop( 'disabled', true );
-      break;
-    case 'Fantastic':
-      if ( !$( '#theme-extra' ).length )
-        appendFantastic();
-      $('#theme-vapor').prop( 'disabled', false );
-      $('#theme-extra').prop( 'disabled', false );
-      break;
-  }
-})
-
-function appendFantastic() {
-  var extra = document.createElement('link');
-  extra.rel = 'stylesheet';
-  extra.type = 'text/css';
-  extra.href = 'extra.css';
-  extra.id = 'theme-extra';
-  extra.disabled = true;
-  $( 'head' ).append(extra);
 }
