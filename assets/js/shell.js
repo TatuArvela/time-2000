c30e33878fe7826204e8daf09f804be300593cff
Tatu Arvela <tatu.arvela@gmail.com>
2019-01-02 01:16:56 +0200

    Custom time input & select

---
+++ assets/js/shell.js
@@ -0,0 +1,175 @@
+/*
+  Window system
+*/
+
+function makeActive(event) {
+  document.querySelectorAll('.window.active')[0].classList.remove('active');
+  if ($(this) != null) {
+    $(this).addClass('active');
+  }
+ else {
+    event.target.classList.add('active');
+  }
+}
+
+var windows = document.querySelectorAll('.window');
+for (let i = 0; i < windows.length; i++) {
+  windows[i].onmousedown = makeActive;
+}
+
+$('.window').draggable({
+  start: makeActive,
+  handle: 'header',
+  containment: 'body'
+});
+
+$('#sakura-time').resizable({
+  start: makeActive,
+  containment: 'body'
+});
+
+
+/* 
+  Time inputs
+*/
+
+function createTimeButtons(input) {
+  const increase = document.createElement('button');
+  increase.setAttribute('class', 'time-button time-increase');
+  increase.setAttribute('id', 'inc-' + input.getAttribute('id'));
+  increase.addEventListener('mousedown', function (event) {
+    event.preventDefault();
+    event.target.classList.add('active');
+    handleTimeButtonClick(event.target.getAttribute('id'));
+  });
+  increase.addEventListener('mouseup', function (event) {
+    event.target.classList.remove('active');
+  });
+
+  const decrease = document.createElement('button');
+  decrease.setAttribute('class', 'time-button time-decrease');
+  decrease.setAttribute('id', 'dec-' + input.getAttribute('id'));
+  decrease.addEventListener('mousedown', function (event) {
+    event.preventDefault();
+    event.target.classList.add('active');
+    handleTimeButtonClick(event.target.getAttribute('id'));
+  });
+  decrease.addEventListener('mouseup', function (event) {
+    event.target.classList.remove('active');
+  });
+
+  input.after(decrease);
+  input.after(increase);
+}
+
+function handleTimeInput(e) {
+  let values = e.target.value.split(':');
+  if (values.length != 2) {
+    e.target.value = "00:00";
+    return;
+  }
+
+  let start = document.activeElement.selectionStart;
+  let end = document.activeElement.selectionEnd;
+  console.log(start, end);
+
+  values[0] = values[0].trim().substring(0, 2).padStart(2, ' ');
+  values[1] = values[1].trim().substring(0, 2).padStart(2, ' ');
+  e.target.value = values[0] + ':' + values[1];
+
+  if (start === 1 && end === 1) {
+    start = 2;
+    end = 2;
+  }
+  if (start === 4 && end === 4) {
+    start = 6;
+    end = 6;
+  }
+
+  document.activeElement.setSelectionRange(start, end);
+}
+
+function handleTimeLeave(e) {
+  let values = e.target.value.split(':');
+
+  if (values.length != 2) {
+    e.target.value = "00:00";
+    return;
+  }
+
+  values[0] = values[0].trim().substring(0, 2).padStart(2, ' ');
+  values[1] = values[1].trim().substring(0, 2).padStart(2, ' ');
+  if (!parseInt(values[0]) || values[0] > 23 || values[0] < 0) values[0] = "00";
+  if (!parseInt(values[1]) || values[1] > 59 || values[1] < 0) values[0] = "00";
+  e.target.value = moment(values[0] + ':' + values[1], "HH:mm").format('HH:mm');
+}
+
+function handleTimeKeypress(e) {
+  if (e.keyCode == 38 || e.keyCode == 40) {
+    let start = document.activeElement.selectionStart;
+    let end = document.activeElement.selectionEnd;
+
+    let time = moment(e.target.value, 'HH:mm');
+
+    let unit = (end < 3) ? 'hours' : 'minutes';
+    (e.keyCode == 38) ? time.add(1, unit) : time.subtract(1, unit);
+    e.target.value = time.format('HH:mm');
+
+    document.activeElement.setSelectionRange(start, end);
+  }
+}
+
+function handleTimeButtonClick(eventTarget) {
+  if (window.getSelection &&
+      document.activeElement &&
+      (document.activeElement.getAttribute('data-type') == 'time')
+  ) {
+    let [method, target] = eventTarget.split("-");
+    let activeInput = document.activeElement;
+    let time = moment(activeInput.value, "HH:mm");
+    if (activeInput.getAttribute('id') === target &&
+        time.isValid()
+    ) {
+      let start = activeInput.selectionStart;
+      let end = activeInput.selectionEnd;
+
+      let unit = (end < 3) ? "hours" : "minutes";
+      (method === "inc") ? time.add(1, unit) : time.subtract(1, unit);
+      activeInput.value = time.format('HH:mm');
+
+      activeInput.dispatchEvent(new Event('change'));
+      activeInput.setSelectionRange(start, end);
+    }
+  }
+}
+
+var timeInputs = document.querySelectorAll('input[data-type="time"]');
+for (let i = 0; i < timeInputs.length; i++) {
+  createTimeButtons(timeInputs[i]);
+  timeInputs[i].oninput = handleTimeInput;
+  timeInputs[i].onkeypress = handleTimeKeypress;
+  timeInputs[i].onblur = handleTimeLeave;
+}
+
+
+/*
+  Theme switcher
+*/
+
+function setTheme(theme) {
+  document.getElementById('theme').setAttribute('href', 'themes/' + theme + '.css');
+}
+
+$('#theme-selector').selectmenu({
+  width: 200,
+  select: function (event) {
+    if (event.target.value) {
+      setTheme(event.target.value);
+    }
+   else {
+      setTheme('');
+    }
+  }
+});
+
+setTheme(document.getElementById('theme-selector').value);
--- assets/js/time2k.js
+++ assets/js/time2k.js
@@ -1,55 +1,26 @@
 /*
-  Shell
-*/
-
-function makeWindowActive() {
-  $('.active').removeClass('active');
-  $(this).addClass('active');
-}
-
-$(document).ready(function() {
-  $('.window').draggable({
-    start: makeWindowActive,
-    handle: 'header',
-    containment: 'body'
-  });
-  
-  $('.window').click(makeWindowActive)  
-
-  $('#sakura-time').resizable({
-    start: makeWindowActive,
-    containment: 'body'
-  });
-});
-
-
-/*
-  Theme switcher
-*/
-
-function setTheme(theme) {
-  $('#theme').prop('href', 'themes/' + theme +'.css');
-}
-
-$('#theme-selector').change(function() {
-  if ($(this).val()) {
-    $('#theme').prop('disabled', false);
-    setTheme($(this).val());
-  }
-  else {
-    $('#theme').prop('disabled', true);
-    setTheme('');
-  }
-})
-
-$(document).ready(function() {
-  setTheme($('#theme-selector').val())
-});
-
-
-/*
   Sakura Time 2000
 */
+
+var defaults = {
+  start:    "08:30",
+  lunch:    "00:30",
+  stop:     "16:30",
+  breaks:   "00:00",
+  task1:    "00:00",
+  task2:    "00:00",
+  task3:    "00:00",
+  task4:    "00:00",
+  task5:    "00:00",
+  task6:    "00:00",
+  planned:  "07:30"
+}
+
+function setDefaults() {
+  for (var key in defaults) {
+    document.getElementById(key).value = defaults[key];
+  }
+}
 
 function getValue(id) {
   var value = document.getElementById(id).value;
@@ -60,7 +31,7 @@ function subtractTime(time1, time2) {
   return moment.utc(moment(time1, 'HH:mm').diff(moment(time2, 'HH:mm'))).format('HH:mm');
 }
 
-function inputChangeHandler() {
+function calculateTimes() {
   var totalTime = subtractTime(getValue('stop'), getValue('start'));
   totalTime = subtractTime(totalTime, getValue('lunch'));
   totalTime = subtractTime(totalTime, getValue('breaks'));
@@ -77,7 +48,7 @@ function inputChangeHandler() {
     document.getElementById('differenceTitle').innerHTML = 'Missing time';
     var timeDifference = subtractTime(getValue('planned'), totalTime);
   }
-  else {
+ else {
     document.getElementById('differenceTitle').innerHTML = 'Overtime';
     var timeDifference = subtractTime(totalTime, getValue('planned'));
   }
@@ -87,10 +58,27 @@ function inputChangeHandler() {
   document.getElementById('difference').value = timeDifference;
 }
 
-inputChangeHandler();
+setDefaults();
+calculateTimes();
 
-var inputs = document.getElementsByTagName('input');
+var inputs = document.querySelectorAll('input[data-type="time"]');
 for (var input in inputs) {
-  if (inputs[input].addEventListener != null)
-    inputs[input].addEventListener('change', inputChangeHandler);
+  if (inputs[input].addEventListener != null) {
+    inputs[input].addEventListener('keyup', function(e) {
+      calculateTimes(e);
+    });
+  }
 }
+var buttons = document.querySelectorAll('time-button');
+for (var button in buttons) {
+  if (buttons[button].addEventListener != null) {
+    buttons[button].addEventListener('click', function(e) {
+      calculateTimes(e);
+    });
+  }
+}
+
+// Exit prompt
+// window.onbeforeunload = function() {
+//   return true;
+// };
