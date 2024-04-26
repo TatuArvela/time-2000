e478761e17e2c23008e8a5616021b1f3d12514fb
Tatu Arvela <tatu.arvela@nitor.com>
2024-04-26 14:23:44 +0300

    configure eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,33 @@
-import {
-  Wisp,
-  ThemeSwitcher,
-} from "@tatuarvela/wisp";
+import { ThemeSwitcher, Wisp } from '@tatuarvela/wisp';
 import {
   vaporwin,
   win2k,
   win9x16Bit,
   win9x16Colors,
   win9x24Bit,
+  win9x32Bit,
   win9x256Colors,
-  win9x32Bit
-} from "@tatuarvela/wisp-win-classic-theme";
-import Time from "./Time.tsx";
+} from '@tatuarvela/wisp-win-classic-theme';
 
-
+import Time from './Time.tsx';
 
 function App() {
   return (
-    <Wisp themes={[
-      win9x16Colors,
-      win9x256Colors,
-      win9x16Bit,
-      win9x24Bit,
-      win9x32Bit,
-      win2k,
-      vaporwin,
-    ]}>
+    <Wisp
+      themes={[
+        win9x16Colors,
+        win9x256Colors,
+        win9x16Bit,
+        win9x24Bit,
+        win9x32Bit,
+        win2k,
+        vaporwin,
+      ]}
+    >
       <ThemeSwitcher height={50} />
       <Time />
     </Wisp>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/icons/index.ts
+++ src/icons/index.ts
@@ -1,26 +1,27 @@
-import {Icon} from "@tatuarvela/wisp";
+import { Icon } from '@tatuarvela/wisp';
+
 import iconExport from './assets/icon-export.png';
 import iconNew from './assets/icon-new.png';
 import iconOpen from './assets/icon-open.png';
-import iconSave from './assets/icon-save.png'
-import iconTime from './assets/icon-time.png'
+import iconSave from './assets/icon-save.png';
+import iconTime from './assets/icon-time.png';
 
 const icons: Record<string, Icon> = {
   time: {
     16: iconTime,
   },
   export: {
-    16: iconExport
+    16: iconExport,
   },
   new: {
-    16: iconNew
+    16: iconNew,
   },
   open: {
-    16: iconOpen
+    16: iconOpen,
   },
   save: {
-    16: iconSave
-  }
-}
+    16: iconSave,
+  },
+};
 
 export default icons;
